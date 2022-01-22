25c2dd0fa94be6621c5bd3f707296c9de139b8ad
Jean Michael <jeanmmclaudino@gmail.com>
2022-01-21 23:35:29 -0300

    Add project files.

---
+++ WebPostgreSQL/Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebPostgreSQL.Models
+{
+    public class Contexto : DbContext
+    {
+        //Contrutor 
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ WebPostgreSQL/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+
+namespace WebPostgreSQL.Models
+{
+
+    [Table("Produto")] //Nome da Tabela no banco 
+    public class Produto
+    {
+        [Column("Id")] //Nome da coluna
+        [Display(Name = "Código")] //Nome que vai aparecer na tela do CRUD
+        public int Id { get; set; }
+
+        [Column("Nome")] //Nome da coluna
+        [Display(Name = "Nome")] //Nome que vai aparecer na tela do CRUD
+        public string Nome { get; set; }
+    }
+}
